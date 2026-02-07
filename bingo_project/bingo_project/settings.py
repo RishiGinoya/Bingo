32f2036e57e0d7818ca94b43af3e4ba430190d83
Rishi80000 <rishiginoya83886.com>
2026-02-07 19:05:19 +0530

    Fix CSRF configuration for Render deployment

--- bingo_project/bingo_project/settings.py
+++ bingo_project/bingo_project/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-e#1p^oib@s8kefd%lno7873j8@zdf&w=4(l-)ped84kqryijsy')
+SECRET_KEY = 'django-insecure-e#1p^oib@s8kefd%lno7873j8@zdf&w=4(l-)ped84kqryijsy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = True
 
-ALLOWED_HOSTS = ["https://bingo-p3pu.onrender.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ["https://bingo-p3pu.onrender.com", "https://127.0.0.1", "https://localhost"]
 
 
